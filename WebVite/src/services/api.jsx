49799586719257f89cd6984747480306ffeaeb0a
Raquel <100868145+raquelm16@users.noreply.github.com>
2023-11-17 12:23:29 -0300

    Delete WebVite directory

--- WebVite/src/services/api.jsx
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-
-export const api = axios.create({
-    // Endereço da API - localhost
-    //baseURL: "http://localhost:3333"
-    // Disponível pelo Render
-    baseURL: "https://mynotes-xjhu.onrender.com"
-})
